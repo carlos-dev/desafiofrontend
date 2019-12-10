7407204aa7981063d6b6b6bffd91e7a4175eb494
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-12-10 02:49:53 -0300

    configurando rota

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 
-const App = () => {
-  return (
-    <View style={styles.content}>
-      <Text>test</Text>
-    </View>
-  );
-};
+import logo from '../../assets/img/Logo_telaInicial.png';
+
+const { navigate } = this.props.navigation;
+export default class App extends React.Component {
+
+
+  render() {
+    return (
+      <View style={styles.content}>
+        <Image source={logo} style={styles.logo} />
+
+        <TextInput
+          placeholder="E-mail"
+          style={styles.input}
+          placeholderTextColor="#273e64"
+        />
+
+        <TextInput
+          placeholder="CPF"
+          style={styles.input}
+          placeholderTextColor="#273e64"
+        />
+
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={navigate('Main')}
+          >
+          <Text style={styles.textBtn}>ENTRAR</Text>
+          <Text style={styles.arrow}>></Text>
+        </TouchableOpacity>
+        <Text style={styles.textPassword}>Esqueceu a senha?</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   content: {
@@ -15,7 +50,50 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 50,
+  },
+
+  logo: {
+    width: '70%',
+    resizeMode: 'contain',
+  },
+
+  input: {
+    width: '100%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    marginVertical: 5,
+  },
+
+  btn: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 30,
+    backgroundColor: '#f05f3c',
+    width: '100%',
+    marginTop: 3,
+  },
+
+  textBtn: {
+    color: '#fff',
+    paddingVertical: 12,
+    fontWeight: 'bold',
+  },
+
+  arrow: {
+    textAlign: 'right',
+    color: '#fff',
+    paddingLeft: 30,
+  },
+
+  textPassword: {
+    display: 'flex',
+    alignSelf: 'flex-end',
+    marginTop: 5,
+    textAlign: 'right',
+    fontSize: 13,
   },
 });
-
-export default App;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,22 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import Login from '../pages/login';
 import Main from '../pages/main';
 
-const Routes = createAppContainer(
-  createSwitchNavigator({
-    Login,
-    Main,
-  }),
-);
+const Routes = createStackNavigator({
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  Main: {
+    screen: Main,
+    navigationOptions: {
+      headerTitle: null,
+    },
+  },
+});
 
-export default Routes;
+export default createAppContainer(Routes);
