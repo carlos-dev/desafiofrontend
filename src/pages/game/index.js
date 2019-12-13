e19414683debdab83d0061b64c6aa08a0929a6c0
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-12-13 08:43:44 -0300

    criando o component game

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, TouchableOpacity, ImageBackground
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 
-import Navbar from '../../components/navbar'
+import Navbar from '../../components/navbar';
 import img_principal_0 from '../../assets/img/Img_principal_0.png';
 import img_principal_1 from '../../assets/img/Img_principal_1.png';
+import arrow from '../../assets/img/arrow.png';
 
 const Game = () => (
   <View>
@@ -12,25 +19,22 @@ const Game = () => (
 
     <View style={styles.mainContent}>
       <View style={styles.play}>
-        <ImageBackground
-          source={img_principal_0}
-          style={styles.backgroundGame}
-        >
-          <Text style={styles.textBtn}>Jogar</Text>
+        <ImageBackground source={img_principal_0} style={styles.backgroundGame}>
+          <Text style={styles.textBackground}>Jogar</Text>
         </ImageBackground>
       </View>
-      
+
       <View style={styles.ranking}>
-        <ImageBackground
-          source={img_principal_1}
-          style={styles.backgroundGame}
-        >
-          <Text style={styles.textBtn}>Ranking</Text>
+        <ImageBackground source={img_principal_1} style={styles.backgroundGame}>
+          <Text style={[styles.textBackground, styles.textRanking]}>
+            Ranking
+          </Text>
         </ImageBackground>
       </View>
 
       <TouchableOpacity style={styles.btnBack}>
-        <Text>Voltar</Text>
+        <Image source={arrow} style={styles.arrow} />
+        <Text style={styles.textBtn}>Voltar</Text>
       </TouchableOpacity>
     </View>
   </View>
@@ -42,11 +46,11 @@ const styles = StyleSheet.create({
   },
 
   play: {
-    height: '40%'
+    height: '40%',
   },
-  
+
   ranking: {
-    height: '60%'
+    height: '60%',
   },
 
   backgroundGame: {
@@ -54,23 +58,51 @@ const styles = StyleSheet.create({
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'flex-end'
+    alignItems: 'flex-end',
   },
 
-  textBtn: {
+  textBackground: {
     fontSize: 24,
     textTransform: 'uppercase',
     backgroundColor: '#fff',
+    color: '#1e314e',
     paddingHorizontal: 20,
-    paddingVertical: 15
+    paddingVertical: 15,
+  },
+
+  textRanking: {
+    position: 'relative',
+    bottom: 60,
   },
 
   btnBack: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
     position: 'absolute',
-    backgroundColor: '#44635a',
-    height: 40,
-    bottom: 0
-  }
+    bottom: 120,
+    backgroundColor: '#1e314e',
+    borderTopLeftRadius: 70,
+    height: 110,
+    width: '100%',
+  },
+
+  textBtn: {
+    textTransform: 'uppercase',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 30,
+    lineHeight: 90,
+    paddingLeft: 30,
+    alignSelf: 'center',
+  },
+
+  arrow: {
+    position: 'absolute',
+    left: 35,
+    width: 18,
+    alignSelf: 'center',
+  },
 });
 
 export default Game;
